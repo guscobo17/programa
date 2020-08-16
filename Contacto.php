ccc829a81b19ec314dfed0bed6825b8bc2e65696
Gcobo <mac71g@gmail.com>
2020-08-15 21:04:19 -0300

    e

--- Contacto.php
+++ Contacto.php
@@ -3,8 +3,10 @@
 echo "Contacto  ";
 =======
 echo "Contacto";
-echo "Contacto1";
+echo "Contacto  1";
 echo "Contacto 2";
+
+echo Contacto 3;
 >>>>>>> 33be8bbec7bd1a10e2bcb5fbcdd2dfc9d9381f2e
 
 ?>
