e6cae9939882531c034c1bb93de2340b08839c57
Gcobo <mac71g@gmail.com>
2020-08-15 21:32:53 -0300

    jj

--- Contacto.php
+++ Contacto.php
@@ -1,17 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 echo "Contacto  ";
-=======
-echo "Contacto";
-<<<<<<< HEAD
-echo "Contacto  1";
-echo "Contacto 2";
-=======
-
-
->>>>>>> 873075f56c691e2af9139693008924bd7acf6de7
-
-echo Contacto 3;
->>>>>>> 33be8bbec7bd1a10e2bcb5fbcdd2dfc9d9381f2e
 
 ?>
